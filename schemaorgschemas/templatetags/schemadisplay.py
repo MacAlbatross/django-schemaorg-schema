8ce3444ee3eed60236640d8809b92933d6d48454
Paul Fleetwood <contact@paulfleetwood.co.uk>
2014-09-03 12:50:56 +0100

    add re-added those needed for end users

--- schemaorgschemas/templatetags/schemadisplay.py
+++ schemaorgschemas/templatetags/schemadisplay.py
@@ -20,7 +20,7 @@ def schemaprop(item, field_name=None):
             store_value = getattr(item, cut_name)
             field_name = cut_name
         except:
-            #print "SchemaError, field_name not in SchemaFields"
+            print "SchemaError, field_name not in SchemaFields"
     if "." in field_name:
         find_point = field_name.find(".")
         try:
@@ -28,11 +28,11 @@ def schemaprop(item, field_name=None):
             new_field_name = field_name[find_point + 1:]
             return schemaprop(new_item, new_field_name)
         except:
-            #print "SchemaError"
+            print "SchemaError"
     try:
         store_value = getattr(item, field_name)
     except:
-        #print "SchemaError, field_name not in SchemaFields"
+        print "SchemaError, field_name not in SchemaFields"
     schema = getattr(item.SchemaFields, field_name)
     format_as = schema._format_as
     if (schema._format_as == 'ForeignKey'):
