7c5d1c8f52445242d189955b668efd122fed3c1c
Paul Fleetwood <contact@paulfleetwood.co.uk>
2015-06-09 12:00:32 +0100

    version no change
    forgot to change v number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'READ.md')) as readme:
 
 setup(
     name='django-schemaorg-schema',
-    version='1.0.1',
+    version='1.0.2',
     packages=['schemaorgschemas'],
     include_package_data=True,
     license='BSD License',  # example license
